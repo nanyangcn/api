6ca61294456363ac3fe3421823148c155ddc09a5
Yang Nan <nanyangcn@gmail.com>
2022-06-10 15:35:47 +0300

    Set eslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!!!');
+import app from './app';
+import config from './util/config';
+
+const { PORT } = config;
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
